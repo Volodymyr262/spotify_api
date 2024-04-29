d36e307bfa69cb6026d27198b368389b24c78956
Volodymyr262 <volodiahedz26@gmail.com>
2024-04-29 20:56:53 +0200

    comments

--- pastebin/base/views.py
+++ pastebin/base/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Room
 
 
-# Create your views here.
+# create room obj and redirect to the room page
 def create_room(request):
     room = Room.objects.create()  # Automatically create a new room
     return redirect('room_detail', room_id=room.room_id)
 
 
+# room page
 def room_detail(request, room_id):
     room = get_object_or_404(Room, room_id=room_id)
     return render(request, 'room_detail.html', {'room': room})
