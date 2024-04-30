e4aede78e03b328324dcf91205a35bac4aa7765e
Volodymyr262 <volodiahedz26@gmail.com>
2024-04-30 18:07:08 +0200

    upgrage consumer

--- pastebin/base/consumer.py
+++ pastebin/base/consumer.py
@@ -1,13 +1,19 @@
-from time import sleep
-
 from channels.generic.websocket import WebsocketConsumer
 import json
 from random import randint
+from time import sleep
 
 
 class WSConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
-        for i in range(1000):
-            self.send(json.dumps({'message': randint(1, 100)}))
+        self.send_message()
+
+    def disconnect(self, close_code):
+        pass
+
+    def send_message(self):
+        while True:
+            message = json.dumps({'message': randint(1, 100)})
+            self.send(message)
             sleep(1)
