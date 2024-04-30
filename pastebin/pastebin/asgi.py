5a7fa4416a0719ce59ef00a5ee0c7ef34f47e609
Volodymyr262 <volodiahedz26@gmail.com>
2024-04-30 17:47:56 +0200

    set the project for websockets

--- pastebin/pastebin/asgi.py
+++ pastebin/pastebin/asgi.py
@@ -9,8 +9,17 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from base.routing import ws_urlpatterns
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pastebin.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(URLRouter(ws_urlpatterns))
+})
+
+
