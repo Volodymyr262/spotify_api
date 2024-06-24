ef7c53aaf722b011468eafce85fd2c682cfc7e0f
Volodymyr262 <volodiahedz26@gmail.com>
2024-06-24 20:54:39 +0200

    added some comments for js code

--- pastebin/base/static/js/editor.js
+++ pastebin/base/static/js/editor.js
@@ -1,24 +1,38 @@
 document.getElementById('download-button').addEventListener('click', function() {
-            const textToWrite = editor.getValue();
-            const textFileAsBlob = new Blob([textToWrite], { type: 'text/plain' });
-            const fileNameToSaveAs = "editor_content.txt";
+    // Get the current value from the editor
+    const textToWrite = editor.getValue();
 
-            const downloadLink = document.createElement("a");
-            downloadLink.download = fileNameToSaveAs;
-            downloadLink.innerHTML = "Download File";
-            if (window.webkitURL != null) {
-                downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
-            } else {
-                downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
-                downloadLink.onclick = destroyClickedElement;
-                downloadLink.style.display = "none";
-                document.body.appendChild(downloadLink);
-            }
+    // Create a new Blob object containing the text, with type 'text/plain'
+    const textFileAsBlob = new Blob([textToWrite], { type: 'text/plain' });
 
-            downloadLink.click();
-        });
+    const fileNameToSaveAs = "editor_content.txt";
 
-        function destroyClickedElement(event) {
-            document.body.removeChild(event.target);
-        }
+    // Create a new anchor element to facilitate the download
+    const downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
 
+    downloadLink.innerHTML = "Download File";
+
+    // Check if the browser supports the webkitURL API
+    if (window.webkitURL != null) {
+        // Use webkitURL to create an object URL for the Blob and set it as the href attribute
+        downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+    } else {
+        // Use the standard URL API to create an object URL for the Blob and set it as the href attribute
+        downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+
+        downloadLink.onclick = destroyClickedElement;
+
+        // Hide the anchor element from view
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+    }
+
+    // Programmatically click the anchor element to trigger the download
+    downloadLink.click();
+});
+
+// Function to remove the clicked element from the document body
+function destroyClickedElement(event) {
+    document.body.removeChild(event.target);
+}
