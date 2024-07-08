00284f06d22fc1ae80ea406133cbe3c126170a11
Volodymyr262 <volodiahedz26@gmail.com>
2024-07-08 20:51:03 +0200

    settings update

--- CodeTogether/CodeTogether/settings.py
+++ CodeTogether/CodeTogether/settings.py
@@ -121,7 +121,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # These are the directories where Django will search for additional static files for development
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "base" / "static",
 ]
 
 # Default primary key field type
